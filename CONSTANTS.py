017d994521e995d395cfb7fa41f852de9463cb51
alokillur <alokillur99@gmail.com>
2024-06-03 18:45:31 +0530

    added

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -3,4 +3,4 @@ DELIMITER = "$"
 api_key = "sk-proj-avmo5cSB2UfIkk2IAGJOT3BlbkFJrqMdgwDLALju05Xtlo01"
 perspective_api_key = 'AIzaSyClOmeNzr50OK01y23Zh7HXqGB2rNHjuxo'
 
-word2vec = 'C:\\Users\\aloky\\OneDrive\\Desktop\\Cryptography\\Secret-Pixels\\GoogleNews-vectors-negative300.bin'
+word2vec = 'C:\\Users\\aloky\\OneDrive\\Desktop\\Secret-Pixels\\GoogleNews-vectors-negative300.bin'
