fb3b40bc9362036c9fa14fe786f1c1b5195fa135
alokillur <alokillur99@gmail.com>
2024-06-09 09:04:42 +0530

    done n dusted

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,7 +1,7 @@
 HEADER_SIZE = 54  
 DELIMITER = "$"   
 
-api_key = "sk-proj-EamyqyLpDgLjDmGBbHMzT3BlbkFJT4YZIhgxfALEa7N1gZNN"
-perspective_api_key = 'AIzaSyClOmeNzr50OK01y23Zh7HXqGB2rNHjuxo'
+api_key = ""
+perspective_api_key = ''
 
 word2vec = 'C:\\Users\\aloky\\OneDrive\\Desktop\\Secret-Pixels\\GoogleNews-vectors-negative300.bin'
